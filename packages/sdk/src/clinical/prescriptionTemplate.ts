d9281f7cec0d924a0324cfe4fd0e57e0c743ddcd
Josh Knapp <joshknappp@gmail.com>
2023-01-17 10:28:43 -0800

    Add update prescription template to sdk and react provider (#62)
    * add react mutations, types
    
    * add to context
    
    * sdk changes

--- packages/sdk/src/clinical/prescriptionTemplate.ts
+++ packages/sdk/src/clinical/prescriptionTemplate.ts
@@ -1,47 +1,44 @@
-import {
-    ApolloClient,
-    DocumentNode,
-    gql,
-    NormalizedCacheObject,
-  } from "@apollo/client";
-import { PRESCRIPTION_TEMPLATE_FIELDS } from "../fragments";
-import { PrescriptionTemplate } from "../types";
-  import { makeMutation, makeQuery } from "../utils";
+import { ApolloClient, DocumentNode, gql, NormalizedCacheObject } from '@apollo/client'
+import { PRESCRIPTION_TEMPLATE_FIELDS } from '../fragments'
+import { PrescriptionTemplate } from '../types'
+import { makeMutation } from '../utils'
 
-  export interface CreatePrescriptionTemplateOptions {
-    fragment?: Record<string, DocumentNode>;
-  }
+export interface CreatePrescriptionTemplateOptions {
+  fragment?: Record<string, DocumentNode>
+}
 
-  export interface DeletePrescriptionTemplateOptions {
-    fragment?: Record<string, DocumentNode>;
-  }
-  
+export interface UpdatePrescriptionTemplateOptions {
+  fragment?: Record<string, DocumentNode>
+}
+
+export interface DeletePrescriptionTemplateOptions {
+  fragment?: Record<string, DocumentNode>
+}
+
+/**
+ * Contains various methods for Photon Prescription Templates
+ */
+export class PrescriptionTemplateQueryManager {
+  private apollo: ApolloClient<undefined> | ApolloClient<NormalizedCacheObject>
+
   /**
-   * Contains various methods for Photon Prescription Templates
+   * @param apollo - An Apollo client instance
    */
-  export class PrescriptionTemplateQueryManager {
-    private apollo: ApolloClient<undefined> | ApolloClient<NormalizedCacheObject>;
-  
-    /**
-     * @param apollo - An Apollo client instance
-     */
-    constructor(
-      apollo: ApolloClient<undefined> | ApolloClient<NormalizedCacheObject>
-    ) {
-      this.apollo = apollo;
+  constructor(apollo: ApolloClient<undefined> | ApolloClient<NormalizedCacheObject>) {
+    this.apollo = apollo
+  }
+
+  /**
+   * Creates a new prescription template
+   * @param options - Query options
+   * @returns
+   */
+  public createPrescriptionTemplate({ fragment }: CreatePrescriptionTemplateOptions) {
+    if (!fragment) {
+      fragment = { PrescriptionTemplateFields: PRESCRIPTION_TEMPLATE_FIELDS }
     }
-  
-    /**
-     * Creates a new prescription template
-     * @param options - Query options
-     * @returns
-     */
-    public createPrescriptionTemplate({ fragment }: CreatePrescriptionTemplateOptions) {
-      if (!fragment) {
-        fragment = { PrescriptionTemplateFields: PRESCRIPTION_TEMPLATE_FIELDS };
-      }
-      let [fName, fValue] = Object.entries(fragment)[0];
-      const CREATE_PRESCRIPTION_TEMPLATE = gql`
+    let [fName, fValue] = Object.entries(fragment)[0]
+    const CREATE_PRESCRIPTION_TEMPLATE = gql`
         ${fValue}
         mutation createPrescriptionTemplate(
           $catalogId: ID!,
@@ -68,24 +65,66 @@ import { PrescriptionTemplate } from "../types";
             ...${fName}
         }
       }
-      `;
-      return makeMutation<{ createPrescriptionTemplate: PrescriptionTemplate } | undefined | null>(
-        this.apollo,
-        CREATE_PRESCRIPTION_TEMPLATE
-      );
-    }
+      `
+    return makeMutation<{ createPrescriptionTemplate: PrescriptionTemplate } | undefined | null>(
+      this.apollo,
+      CREATE_PRESCRIPTION_TEMPLATE
+    )
+  }
 
-    /**
-     * Deletes an existing prescription template
-     * @param options - Query options
-     * @returns
-     */
-     public deletePrescriptionTemplate({ fragment }: DeletePrescriptionTemplateOptions) {
-      if (!fragment) {
-        fragment = { PrescriptionTemplateFields: PRESCRIPTION_TEMPLATE_FIELDS };
+  /**
+   * Updates a prescription template
+   * @param options - Query options
+   * @returns
+   */
+  public updatePrescriptionTemplate({ fragment }: UpdatePrescriptionTemplateOptions) {
+    if (!fragment) {
+      fragment = { PrescriptionTemplateFields: PRESCRIPTION_TEMPLATE_FIELDS }
+    }
+    let [fName, fValue] = Object.entries(fragment)[0]
+    const UPDATE_PRESCRIPTION_TEMPLATE = gql`
+        ${fValue}
+        mutation updatePrescriptionTemplate(
+          $treatmentId: ID!,
+          $dispenseAsWritten: Boolean,
+          $dispenseQuantity: Float,
+          $dispenseUnit: String,
+          $refillsAllowed: Int,
+          $daysSupply: Int,
+          $instructions: String,
+          $notes: String
+        ) {
+          updatePrescriptionTemplate(
+            treatmentId: $treatmentId
+            dispenseAsWritten: $dispenseAsWritten
+            dispenseQuantity: $dispenseQuantity
+            dispenseUnit: $dispenseUnit
+            refillsAllowed: $refillsAllowed
+            daysSupply: $daysSupply
+            instructions: $instructions
+            notes: $notes
+        ) {
+            ...${fName}
+        }
       }
-      let [fName, fValue] = Object.entries(fragment)[0];
-      const DELETE_PRESCRIPTION_TEMPLATE = gql`
+      `
+    return makeMutation<{ updatePrescriptionTemplate: PrescriptionTemplate } | undefined | null>(
+      this.apollo,
+      UPDATE_PRESCRIPTION_TEMPLATE
+    )
+  }
+
+  /**
+   * Deletes an existing prescription template
+   * @param options - Query options
+   * @returns
+   */
+  public deletePrescriptionTemplate({ fragment }: DeletePrescriptionTemplateOptions) {
+    if (!fragment) {
+      fragment = { PrescriptionTemplateFields: PRESCRIPTION_TEMPLATE_FIELDS }
+    }
+    let [fName, fValue] = Object.entries(fragment)[0]
+    const DELETE_PRESCRIPTION_TEMPLATE = gql`
         ${fValue}
         mutation deletePrescriptionTemplate(
           $catalogId: ID!,
@@ -98,10 +137,10 @@ import { PrescriptionTemplate } from "../types";
             ...${fName}
         }
       }
-      `;
-      return makeMutation<{ deletePrescriptionTemplate: PrescriptionTemplate } | undefined | null>(
-        this.apollo,
-        DELETE_PRESCRIPTION_TEMPLATE
-      );
-    }
+      `
+    return makeMutation<{ deletePrescriptionTemplate: PrescriptionTemplate } | undefined | null>(
+      this.apollo,
+      DELETE_PRESCRIPTION_TEMPLATE
+    )
   }
+}
